b4449f8c4e53988c1f749d3a494e0ec151b32bb4
Fabian Andres Marin Montoya <fabianandres.marin01@gmail.com>
2026-08-19 14:47:57 -0500

    Diseño de la vista horario

--- MiProyecto/components/haptic-tab.tsx
+++ MiProyecto/components/haptic-tab.tsx
@@ -1,6 +1,7 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
+
 import * as Haptics from 'expo-haptics';
+import { PlatformPressable } from 'expo-router/react-navigation';
 
 export function HapticTab(props: BottomTabBarButtonProps) {
   return (
